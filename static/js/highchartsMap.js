a4c08b43895cd56d75afaabfd22961fd53cfbec9
Jacob de Vries <mrjacobdevries@gmail.com>
2021-04-27 15:37:38 -0700

    Working potentially on different mapping program.

--- static/js/highchartsMap.js
+++ static/js/highchartsMap.js
@@ -1,33 +1,29 @@
-function stateLatLong(stateAbbr) {
-    switch(stateAbbr) {
-        case stateAbbr = 'AL': return [0,1];
-        default : return [10,10];
-    };
-        
-}
-
-d3.json('http://127.0.0.1:5000/api/states/').then(stateData => {
-    console.log(stateData)
+d3.json('http://127.0.0.1:5000/api/states/').then((stateData) => {
     const geoJSON = {
         'type': 'FeatureCollection',
         'features': []
-      };;
-    stateData.forEach(s => {
-        let stateGeoJson = {
-            'type':'Feature',
-            'properties': {
-                'name': s.state,
-                'highSpeed': s.PopulationWithHighSpeedInternet
-            },
-            'geometry': {
-                'type': 'Point',
-                'coordinates': stateLatLong(s.state)
+    }; 
+    stateData.forEach(state => {
+        d3.json(`https://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&inFormat=kvp&outFormat=json&location=${state.state}%2C+US&thumbMaps=false`).then(mapQuestData => {
+            let coords = [];
+            if (mapQuestData.results[0].locations[0].adminArea1 === 'US') {
+                coords = [mapQuestData.results[0].locations[0].latLng.lat,mapQuestData.results[0].locations[0].latLng.lng]
+            } else if (mapQuestData.results[0].locations[1].adminArea1 === 'US') {
+                coords = [mapQuestData.results[0].locations[1].latLng.lat,mapQuestData.results[0].locations[1].latLng.lng]
+            };
+            let stateObject = {
+                'type':'Feature',
+                'properties': {
+                    'name': state.state,
+                    'highSpeed': state.PopulationWithHighSpeedInternet
+                },
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': coords
+                }
             }
-        };
-        geoJSON.features.push(stateGeoJson);
-        console.log(stateGeoJson.geometry.coordinates)
+            geoJSON.features.push(stateObject)
+        })
     });
     console.log(geoJSON)
-    // const dataAsGeoJSON;
-    // const data = [], mapData = Highcharts.geojson(dataAsGeoJSON);
-})
+});
