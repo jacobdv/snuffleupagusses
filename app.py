2b235e1d491820713640c0931699fa4732a76ef6
Jacob de Vries <mrjacobdevries@gmail.com>
2021-05-01 09:53:05 -0700

    Requirements.txt changes.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, url_for, jsonify, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from census import Census
+
 # Additional tools for API routes.
 import json
 from bson import json_util
